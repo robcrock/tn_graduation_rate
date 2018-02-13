135a9636d2f17a27495ae2b03d838faffb72ffff
robcrock <rob@robcrock.com>
2018-02-13 18:59:55 +1100

    Series structure.

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ const innerWidth = width - (margin.right + margin.left),
 const svg = d3.select('#chart').append('svg')
     .attr('width', width)
     .attr('height', height);
+
+const xScale = d3.scaleLinear()
+  .range([0, innerWidth]);
+
+d3.csv('tn_four_year_bach.csv', function(error, data) {
+  if (error) throw error;
+
+  // console.log(data);
+
+  const white = data.filter(function(row) {
+    return row.year === '2012' &&
+      row.gender === 'M' &&
+      row.control === 'Private for-profit' &&
+      row.race === 'W';
+  });
+
+  // console.log(white);
+
+  const stack = d3.stack();
+
+  stack.keys(['perc_grad_in_4', 'perc_grad_in_6']);
+
+  const series = stack(white);
+
+  console.log(series);
+
+  // Complete x-scale
+  xScale.domain([0, d3.max(white)])
+
+})
